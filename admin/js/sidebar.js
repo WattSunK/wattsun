b06d21e95f2101f94c2d46294fb396658e96f858
WattSunK <mainakamunyu@gmail.com>
2025-08-04 18:04:23 +0200

    updates

--- admin/js/sidebar.js
+++ admin/js/sidebar.js
@@ -27,7 +27,9 @@ function initSidebarUserInfo() {
   }
 
   userInfoContainer.innerHTML = `
+    <a href="/admin/index.html#dashboard" style="text-decoration:none;color:inherit;">
     <i class="fas fa-user"></i> ${user.name || "Unknown"} (${user.type || "Unknown"})
+  </a>
   `;
 
   const adminLinks = document.querySelectorAll(".admin-only");
